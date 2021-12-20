792257c2e0626e1097237486d300d26cc8b9b124
James Roberts <James@Robertscom.com>
2021-12-20 16:18:02 +1100

    Add XML comments
    CAS-22

--- CleanArchitecture.Services/DependencyInjection/UseCaseElementOptions.cs
+++ CleanArchitecture.Services/DependencyInjection/UseCaseElementOptions.cs
@@ -1,12 +1,15 @@
 ﻿namespace CleanArchitecture.Services.DependencyInjection
 {
 
+    /// <summary>
+    /// The options used to configure a pipe in the Use Case Pipeline.
+    /// </summary>
     public class UseCaseElementOptions
     {
 
         #region - - - - - - Constructors - - - - - -
 
-        public UseCaseElementOptions(Type elementType)
+        internal UseCaseElementOptions(Type elementType)
             => this.ElementType = elementType;
 
         #endregion Constructors
@@ -21,9 +24,19 @@
 
         #region - - - - - - Methods - - - - - -
 
+        /// <summary>
+        /// Registers a service as being required by the pipe.
+        /// </summary>
+        /// <typeparam name="TService">The type of service required by the pipe.</typeparam>
+        /// <returns>Itself.</returns>
         public UseCaseElementOptions AddUseCaseService<TService>()
             => this.AddUseCaseService(typeof(TService));
 
+        /// <summary>
+        /// Registers a service as being required by the pipe.
+        /// </summary>
+        /// <param name="service">The type of service required by the pipe.</param>
+        /// <returns>Itself.</returns>
         public UseCaseElementOptions AddUseCaseService(Type service)
         {
             this.Services.Add(service);
