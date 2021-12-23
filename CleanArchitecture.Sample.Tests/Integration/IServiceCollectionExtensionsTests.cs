08a62655f9ff3c7092431d97a4b7f31675c24158
James Roberts <James@Robertscom.com>
2021-12-23 20:18:13 +1100

    Add integration test to ensure no validation issues with registration
    CAS-33

---
+++ CleanArchitecture.Sample.Tests/Integration/IServiceCollectionExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace CleanArchitecture.Sample.Tests.Integration
+{
+
+    public class IServiceCollectionExtensionsTests
+    {
+
+        #region - - - - - - AddCleanArchitectureServices Tests - - - - - -
+
+        [Fact]
+        public void AddCleanArchitectureServices_NoValidationIssues_DoesNotThrowValidationException()
+        {
+            // Arrange
+            var _ServiceCollection = new ServiceCollection();
+
+            // Act
+            var _Exception = Record.Exception(() => _ = _ServiceCollection.AddCleanArchitectureServices());
+
+            // Assert
+            _ = _Exception.Should().BeNull();
+        }
+
+        #endregion AddCleanArchitectureServices Tests
+
+    }
+
+}
