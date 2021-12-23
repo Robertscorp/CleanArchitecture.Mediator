840dbd91b377f2e9c80fc7792d805b78dba8c3d5
James Roberts <James@Robertscom.com>
2021-12-23 15:07:12 +1100

    Add ValidationException
    CAS-17

---
+++ CleanArchitecture.Services/DependencyInjection/Validation/ValidationException.cs
@@ -0,0 +1,15 @@
+﻿namespace CleanArchitecture.Services.DependencyInjection.Validation
+{
+
+    public class ValidationException : Exception
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        internal ValidationException(string message) : base(message) { }
+
+        #endregion Constructors
+
+    }
+
+}
