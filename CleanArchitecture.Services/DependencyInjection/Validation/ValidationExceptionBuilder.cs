cb9a0d8e73bc1d17eddec5bcd5664748a6c1c959
James Roberts <James@Robertscom.com>
2021-12-23 17:00:40 +1100

    Remove unnecessary condition
    CAS-17

--- CleanArchitecture.Services/DependencyInjection/Validation/ValidationExceptionBuilder.cs
+++ CleanArchitecture.Services/DependencyInjection/Validation/ValidationExceptionBuilder.cs
@@ -14,7 +14,8 @@ namespace CleanArchitecture.Services.DependencyInjection.Validation
 
         #region - - - - - - Properties - - - - - -
 
-        private bool HasValidationFailure => this.m_MissingServices.Any();
+        private bool HasValidationFailure
+            => this.m_MissingServices.Any();
 
         #endregion Properties
 
@@ -28,16 +29,13 @@ namespace CleanArchitecture.Services.DependencyInjection.Validation
             var _StringBuilder = new StringBuilder(32);
             _ = _StringBuilder.AppendLine();
 
-            if (this.m_MissingServices.Any())
+            foreach (var (_InputPort, _MissingServices) in this.m_MissingServices.OrderBy(ipms => ipms.InputPort.Name))
             {
-                foreach (var (_InputPort, _MissingServices) in this.m_MissingServices.OrderBy(ipms => ipms.InputPort.Name))
-                {
-                    _ = _StringBuilder.Append(_InputPort.GetFriendlyName()).AppendLine(" is missing implementations for:");
+                _ = _StringBuilder.Append(_InputPort.GetFriendlyName()).AppendLine(" is missing implementations for:");
 
-                    foreach (var _MissingService in _MissingServices.OrderBy(s => s.Name))
-                        _ = _StringBuilder.Append(" - ").AppendLine(_MissingService.GetFriendlyName());
-                    _ = _StringBuilder.AppendLine();
-                }
+                foreach (var _MissingService in _MissingServices.OrderBy(s => s.Name))
+                    _ = _StringBuilder.Append(" - ").AppendLine(_MissingService.GetFriendlyName());
+                _ = _StringBuilder.AppendLine();
             }
 
             return _StringBuilder.ToString().TrimEnd();
