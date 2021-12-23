fd298bc6451b07b1bc0900024dfd38fcbd6f8560
James Roberts <James@Robertscom.com>
2021-12-23 16:53:25 +1100

    Remove newline
    CAS-17

--- CleanArchitecture.Services/DependencyInjection/Validation/TypeExtensions.cs
+++ CleanArchitecture.Services/DependencyInjection/Validation/TypeExtensions.cs
@@ -16,7 +16,6 @@ namespace CleanArchitecture.Services.DependencyInjection.Validation
         private static string GetGenericArguments(Type type)
             => type.GenericTypeArguments.Select(t => t.GetFriendlyName()).Aggregate((agg, inc) => $"{agg}, {inc}");
 
-
         public static Type GetTypeDefinition(this Type type)
             => type.IsGenericType ? type.GetGenericTypeDefinition() : type;
 
