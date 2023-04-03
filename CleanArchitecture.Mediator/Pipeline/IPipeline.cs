4995c142721b4b69f722b15b3f96214299b2efed
James Roberts <James@Robertscom.com>
2023-04-03 19:59:24 +1000

    Add the Pipeline interface
    #71

---
+++ CleanArchitecture.Mediator/Pipeline/IPipeline.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Mediator.Pipeline
+{
+
+    /// <summary>
+    /// A Pipeline that can be configured and invoked.
+    /// </summary>
+    public interface IPipeline
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        /// <summary>
+        /// Invokes the Pipeline.
+        /// </summary>
+        /// <typeparam name="TOutputPort">The type of Output Port.</typeparam>
+        /// <param name="inputPort">The input to the pipeline.</param>
+        /// <param name="outputPort">The output mechanism for the pipeline.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be cancelled.</param>
+        Task InvokeAsync<TOutputPort>(
+            IUseCaseInputPort<TOutputPort> inputPort,
+            TOutputPort outputPort,
+            CancellationToken cancellationToken);
+
+        #endregion Methods
+
+    }
+
+}
