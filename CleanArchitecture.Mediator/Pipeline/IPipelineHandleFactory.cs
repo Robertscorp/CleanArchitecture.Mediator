cfe069e1695eed6cb60b9fdf76d079b917a25b41
James Roberts <James@Robertscom.com>
2023-04-03 20:05:44 +1000

    Add the Pipeline Handle Factory service
    #71

---
+++ CleanArchitecture.Mediator/Pipeline/IPipelineHandleFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace CleanArchitecture.Mediator.Pipeline
+{
+
+    /// <summary>
+    /// A factory that provides an invokable handle for a Pipeline.
+    /// </summary>
+    public interface IPipelineHandleFactory
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        /// <summary>
+        /// Gets an invokable handle for the Pipeline.
+        /// </summary>
+        /// <typeparam name="TPipeline">The type of Pipeline.</typeparam>
+        /// <returns>The invokable handle for the Pipeline.</returns>
+        PipeHandle GetPipelineHandle<TPipeline>() where TPipeline : IPipeline;
+
+        #endregion Methods
+
+    }
+
+}
