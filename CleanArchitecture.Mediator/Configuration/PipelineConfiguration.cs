351e517ee11bcbe6088f6568a6898913d7b9439f
James Roberts <James@Robertscom.com>
2023-04-03 21:31:57 +1000

    Add package and pipeline configuration
    #71

---
+++ CleanArchitecture.Mediator/Configuration/PipelineConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using CleanArchitecture.Mediator.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CleanArchitecture.Mediator.Configuration
+{
+
+    internal class PipelineConfiguration
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private PipeHandle m_PipeHandle;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        internal PipelineConfiguration(Type pipelineType)
+            => this.PipelineType = pipelineType;
+
+        #endregion Constructors
+
+        #region - - - - - - Properties - - - - - -
+
+        internal Type PipelineType { get; }
+
+        internal List<Type> PipeTypes { get; } = new List<Type>();
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        public PipeHandle GetPipeHandle(ServiceFactory serviceFactory)
+        {
+            if (this.m_PipeHandle == null)
+            {
+                var _PipesByType = serviceFactory.GetService<IEnumerable<IPipe>>().ToDictionary(p => p.GetType());
+
+                this.m_PipeHandle = this.PipeTypes
+                                        .Select(t => _PipesByType.TryGetValue(t, out var _Pipe) ? _Pipe : null)
+                                        .Where(p => p != null)
+                                        .Reverse()
+                                        .Aggregate(
+                                            new PipeHandle(null, null),
+                                            (nextPipeHandle, useCasePipe) => new PipeHandle(useCasePipe, nextPipeHandle));
+            }
+
+            return this.m_PipeHandle;
+        }
+
+        #endregion Methods
+
+    }
+
+}
---
+++ CleanArchitecture.Mediator/Configuration/PipelineConfigurationBuilder.cs
@@ -0,0 +1,44 @@
+﻿using CleanArchitecture.Mediator.Pipeline;
+using System;
+
+namespace CleanArchitecture.Mediator.Configuration
+{
+
+    /// <summary>
+    /// A builder used to configure a pipeline.
+    /// </summary>
+    public class PipelineConfigurationBuilder
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        internal PipelineConfigurationBuilder(Type pipelineType)
+            => this.PipelineConfiguration = new PipelineConfiguration(pipelineType);
+
+        #endregion Constructors
+
+        #region - - - - - - Properties - - - - - -
+
+        internal PipelineConfiguration PipelineConfiguration { get; }
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        /// <summary>
+        /// Adds a Pipe to the Pipeline.
+        /// </summary>
+        /// <typeparam name="TPipe">The type of Pipe to add to the Pipeline.</typeparam>
+        /// <returns>Itself.</returns>
+        public PipelineConfigurationBuilder AddPipe<TPipe>() where TPipe : IPipe
+        {
+            this.PipelineConfiguration.PipeTypes.Add(typeof(TPipe));
+
+            return this;
+        }
+
+        #endregion Methods
+
+    }
+
+}
