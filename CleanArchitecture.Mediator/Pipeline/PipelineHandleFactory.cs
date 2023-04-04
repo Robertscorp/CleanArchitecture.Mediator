1ce72b1aeccf7f91d48538c53fd9e79b03dc322b
James Roberts <James@Robertscom.com>
2023-04-04 18:21:23 +1000

    Add the PipelineHandleFactory implementation
    #71

---
+++ CleanArchitecture.Mediator/Pipeline/PipelineHandleFactory.cs
@@ -0,0 +1,44 @@
+﻿using CleanArchitecture.Mediator.Configuration;
+using System;
+using System.Linq;
+
+namespace CleanArchitecture.Mediator.Pipeline
+{
+
+    /// <summary>
+    /// A factory that provides an invokable handle for a Pipeline.
+    /// </summary>
+    public class PipelineHandleFactory : IPipelineHandleFactory
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly ServiceFactory m_ServiceFactory;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="PipelineHandleFactory"/> class.
+        /// </summary>
+        /// <param name="serviceFactory">The factory used to get the service object of the specified type.</param>
+        public PipelineHandleFactory(ServiceFactory serviceFactory)
+            => this.m_ServiceFactory = serviceFactory ?? throw new ArgumentNullException(nameof(serviceFactory));
+
+        #endregion Constructors
+
+        #region - - - - - - Methods - - - - - -
+
+        PipeHandle IPipelineHandleFactory.GetPipelineHandle<TPipeline>()
+            => this.m_ServiceFactory
+                .GetService<PackageConfiguration>()
+                .PipelineConfigurations
+                .SingleOrDefault(c => Equals(c.PipelineType, typeof(TPipeline)))?
+                .GetPipeHandle(this.m_ServiceFactory);
+
+        #endregion Methods
+
+    }
+
+}
