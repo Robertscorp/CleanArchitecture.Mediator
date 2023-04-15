45179e5b6070458581dad7ee52a7e14412d9d83a
James Roberts <James@Robertscom.com>
2023-04-15 10:07:41 +1000

    Add the default and verification pipelines
    #71

---
+++ CleanArchitecture.Sample/Pipelines/DefaultPipeline.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Mediator;
+
+namespace CleanArchitecture.Sample.Pipelines
+{
+
+    public class DefaultPipeline : Mediator.Pipeline
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        public DefaultPipeline(ServiceFactory serviceFactory) : base(serviceFactory) { }
+
+        #endregion Constructors
+
+    }
+
+}
