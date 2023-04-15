55b043b83766fe7706770e21ac52f9d1a265f81a
James Roberts <James@Robertscom.com>
2023-04-15 10:59:30 +1000

    Register and configure CleanArchitecture.Mediator
    #71

--- CleanArchitecture.Sample/IServiceCollectionExtensions.cs
+++ CleanArchitecture.Sample/IServiceCollectionExtensions.cs
@@ -1,5 +1,11 @@
-﻿using CleanArchitecture.Mediator.DependencyInjection;
-using CleanArchitecture.Sample.Pipeline;
+﻿using CleanArchitecture.Mediator;
+using CleanArchitecture.Mediator.Configuration;
+using CleanArchitecture.Mediator.Internal;
+using CleanArchitecture.Mediator.Pipes;
+using CleanArchitecture.Sample.Infrastructure;
+using CleanArchitecture.Sample.Pipelines;
+using CleanArchitecture.Sample.UseCases.CreateProduct;
+using CleanArchitecture.Sample.UseCases.GetProduct;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArchitecture.Sample
@@ -12,6 +18,51 @@ namespace CleanArchitecture.Sample
 
         public static IServiceCollection AddCleanArchitectureMediator(this IServiceCollection serviceCollection)
         {
+            var _PackageConfiguration = CleanArchitectureMediator.Configure(builder =>
+            {
+                _ = builder.AddPipeline<DefaultPipeline>(pipeline
+                    => pipeline
+                        .AddPipe(async opts =>
+                        {
+                            Console.Write("Invoking Default Pipeline...");
+                            await opts.NextPipeHandle.InvokePipeAsync();
+                            Console.WriteLine(" ...Done!");
+                        })
+                        .AddAuthentication()
+                        .AddAuthorisation<AuthorisationResult>()
+                        .AddValidation<ValidationResult>()
+                        .AddInteractorInvocation());
+
+                _ = builder.AddPipeline<VerificationPipeline>(pipeline
+                    => pipeline
+                        .AddPipe(async opts =>
+                        {
+                            Console.Write("Invoking Verification Pipeline...");
+                            await opts.NextPipeHandle.InvokePipeAsync();
+                            Console.WriteLine(" ...Done!");
+                        })
+                        .AddAuthentication()
+                        .AddAuthorisation<AuthorisationResult>()
+                        .AddValidation<ValidationResult>());
+            });
+
+            _ = serviceCollection.AddSingleton(_PackageConfiguration.GetType(), _PackageConfiguration);
+            _ = serviceCollection.AddScoped<ServiceFactory>(serviceProvider => serviceProvider.GetService);
+
+            // The following services will be handled via ServiceRegistrations (returned in place of _PackageConfiguration)
+            _ = serviceCollection.AddScoped<DefaultPipeline>();
+            _ = serviceCollection.AddScoped<IAuthenticatedClaimsPrincipalProvider, AuthenticatedClaimsPrincipalProvider>();
+            _ = serviceCollection.AddScoped<IPipe, AuthenticationPipe>();
+            _ = serviceCollection.AddScoped<IPipe, AuthorisationPipe<AuthorisationResult>>();
+            _ = serviceCollection.AddScoped<IPipe, InteractorInvocationPipe>();
+            _ = serviceCollection.AddScoped<IPipe, ValidationPipe<ValidationResult>>();
+            _ = serviceCollection.AddScoped<IUseCaseAuthorisationEnforcer<CreateProductInputPort, AuthorisationResult>, CreateProductAuthorisationEnforcer>();
+            _ = serviceCollection.AddScoped<IValidator<CreateProductInputPort, ValidationResult>, CreateProductInputPortValidator>();
+            _ = serviceCollection.AddScoped<IUseCaseInteractor<CreateProductInputPort, ICreateProductOutputPort>, CreateProductInteractor>();
+            _ = serviceCollection.AddScoped<IUseCaseInteractor<GetProductInputPort, IGetProductOutputPort>, GetProductInteractor>();
+            _ = serviceCollection.AddScoped<VerificationPipeline>();
+            _ = serviceCollection.AddSingleton<IPipelineHandleFactory, PipelineHandleFactory>();
+
             return serviceCollection;
         }
 
