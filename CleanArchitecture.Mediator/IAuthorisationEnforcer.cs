7efe2a8f4cb27b713d0bdab95eab6e27e459bcc1
James Roberts <James@Robertscom.com>
2023-04-17 21:22:41 +1000

    Improve XML comment
    #71

--- CleanArchitecture.Mediator/IAuthorisationEnforcer.cs
+++ CleanArchitecture.Mediator/IAuthorisationEnforcer.cs
@@ -17,7 +17,7 @@ namespace CleanArchitecture.Mediator
         #region - - - - - - Methods - - - - - -
 
         /// <summary>
-        /// Determines if the pipeline is allowed to continue with the specified input port.
+        /// Determines if the pipeline is allowed to continue.
         /// </summary>
         /// <param name="inputPort">The input to the pipeline.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be cancelled.</param>
