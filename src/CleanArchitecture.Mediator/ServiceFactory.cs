d6dcf1272509fd28a4a384e427cd84f9e44674d0
James Roberts <James@Robertscom.com>
2025-02-04 21:54:39 +1100

    Improve XML comments
    #84

--- src/CleanArchitecture.Mediator/ServiceFactory.cs
+++ src/CleanArchitecture.Mediator/ServiceFactory.cs
@@ -4,10 +4,10 @@ namespace CleanArchitecture.Mediator
 {
 
     /// <summary>
-    /// The factory used to produce the service object of the specified type.
+    /// The factory used to produce a service instance of the specified type.
     /// </summary>
     /// <param name="serviceType">The type of service to produce.</param>
-    /// <returns>The service that was produced.</returns>
+    /// <returns>The service instance that was produced.</returns>
     public delegate object ServiceFactory(Type serviceType);
 
     /// <summary>
@@ -19,11 +19,11 @@ namespace CleanArchitecture.Mediator
         #region - - - - - - Methods - - - - - -
 
         /// <summary>
-        /// Produces the service object of the specified type.
+        /// Produces a service instance of the specified type.
         /// </summary>
         /// <typeparam name="TService">The type of service to produce.</typeparam>
         /// <param name="serviceFactory">The factory used to produce the service.</param>
-        /// <returns>The service that was produced.</returns>
+        /// <returns>The service instance that was produced.</returns>
         public static TService GetService<TService>(this ServiceFactory serviceFactory)
             => (TService)serviceFactory(typeof(TService));
 
