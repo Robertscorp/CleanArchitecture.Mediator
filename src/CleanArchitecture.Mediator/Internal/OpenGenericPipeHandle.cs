227ebcd77f8bd2958a2b684195b0003d66e0bea8
James Roberts <James@Robertscom.com>
2025-07-19 13:33:52 +1000

    Add a PipeHandle for the generic IPipe interface
    #98

---
+++ src/CleanArchitecture.Mediator/Internal/OpenGenericPipeHandle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Mediator.Internal
+{
+
+    internal class OpenGenericPipeHandle : IPipeHandle
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly IPipeHandle m_NextPipeHandle;
+        private readonly Func<Type, Type, ServiceFactory, object> m_PipeProvider;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        internal OpenGenericPipeHandle(Type openGenericPipeType, Func<Type, Type, Type[]> getClosedGenericParameters, IPipeHandle nextPipeHandle)
+        {
+            this.m_NextPipeHandle = nextPipeHandle;
+
+            var _PipeCache = new ConcurrentDictionary<Type, object>();
+
+            this.m_PipeProvider = (inputPort, outputPort, serviceFactory)
+                => _PipeCache.GetOrAdd(inputPort, _ =>
+                {
+                    try // Try-Catch is the easiest way of determining if all generic constraints are met.
+                    {
+                        return serviceFactory(openGenericPipeType.MakeGenericType(getClosedGenericParameters(inputPort, outputPort)));
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                });
+        }
+
+        #endregion Constructors
+
+        #region - - - - - - Methods - - - - - -
+
+        Task IPipeHandle.InvokePipeAsync<TInputPort, TOutputPort>(
+            TInputPort inputPort,
+            TOutputPort outputPort,
+            ServiceFactory serviceFactory,
+            CancellationToken cancellationToken)
+            => cancellationToken.IsCancellationRequested
+                ? Task.FromCanceled(cancellationToken)
+                : ((IPipe<TInputPort, TOutputPort>)this.m_PipeProvider(typeof(TInputPort), typeof(TOutputPort), serviceFactory))?
+                    .InvokeAsync(inputPort, outputPort, serviceFactory, this.m_NextPipeHandle, cancellationToken)
+                        ?? this.m_NextPipeHandle.InvokePipeAsync(inputPort, outputPort, serviceFactory, cancellationToken);
+
+        #endregion Methods
+
+    }
+
+}
---
+++ test/CleanArchitecture.Mediator.Tests/Unit/Internal/OpenGenericPipeHandleTests .cs
@@ -0,0 +1,98 @@
+﻿using CleanArchitecture.Mediator.Internal;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitecture.Mediator.Tests.Unit.Internal
+{
+
+    public class OpenGenericPipeHandleTests
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly Mock<IPipeHandle> m_MockNextPipeHandle = new();
+        private readonly Mock<IPipe<IInputPort<object>, object>> m_MockPipe = new();
+
+        private readonly IInputPort<object> m_InputPort = new Mock<IInputPort<object>>().Object;
+        private readonly object m_OutputPort = new();
+        private readonly IPipeHandle m_PipeHandle;
+        private readonly ServiceFactory m_ServiceFactory;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public OpenGenericPipeHandleTests()
+        {
+            this.m_PipeHandle = new OpenGenericPipeHandle(typeof(IPipe<,>), (inputPort, outputPort) => new Type[] { inputPort, outputPort }, this.m_MockNextPipeHandle.Object);
+
+            var _MockServiceFactory = new Mock<ServiceFactory>();
+            _ = _MockServiceFactory
+                    .Setup(mock => mock(typeof(IPipe<IInputPort<object>, object>)))
+                    .Returns(this.m_MockPipe.Object);
+
+            this.m_ServiceFactory = _MockServiceFactory.Object;
+        }
+
+        #endregion Constructors
+
+        #region - - - - - - InvokePipeAsync Tests - - - - - -
+
+        [Fact]
+        public async Task InvokePipeAsync_OperationIsCancelled_CancelsImmediately()
+        {
+            // Arrange
+            var _CancellationTokenSource = new CancellationTokenSource();
+
+            // Act
+            _CancellationTokenSource.Cancel();
+
+            var _Actual = this.m_PipeHandle.InvokePipeAsync(this.m_InputPort, this.m_OutputPort, default, _CancellationTokenSource.Token);
+            var _Exception = await Record.ExceptionAsync(() => _Actual);
+
+            // Assert
+            _ = _Actual.Status.Should().Be(TaskStatus.Canceled);
+            _ = _Exception.Should().BeOfType<TaskCanceledException>();
+
+            this.m_MockPipe.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task InvokePipeAsync_PipeDoesNotImplementGenericIPipeInterface_MovesToNextPipe()
+        {
+            // Arrange
+            var _PipeHandle = (IPipeHandle)new OpenGenericPipeHandle(typeof(List<>), (inputPort, outputPort) => new Type[] { typeof(object) }, this.m_MockNextPipeHandle.Object);
+
+            // Act
+            await _PipeHandle.InvokePipeAsync(this.m_InputPort, this.m_OutputPort, this.m_ServiceFactory, default);
+
+            // Assert
+            this.m_MockNextPipeHandle.Verify(mock => mock.InvokePipeAsync(this.m_InputPort, this.m_OutputPort, this.m_ServiceFactory, default), Times.Once());
+
+            this.m_MockPipe.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task InvokePipeAsync_PipeImplementsGenericIPipeInterfaceAndOperationIsNotCancelled_InvokesPipe()
+        {
+            // Arrange
+
+            // Act
+            await this.m_PipeHandle.InvokePipeAsync(this.m_InputPort, this.m_OutputPort, this.m_ServiceFactory, default);
+
+            // Assert
+            this.m_MockPipe.Verify(mock => mock.InvokeAsync(this.m_InputPort, this.m_OutputPort, this.m_ServiceFactory, this.m_MockNextPipeHandle.Object, default), Times.Once());
+
+            this.m_MockPipe.VerifyNoOtherCalls();
+        }
+
+        #endregion InvokePipeAsync Tests
+
+    }
+
+}
