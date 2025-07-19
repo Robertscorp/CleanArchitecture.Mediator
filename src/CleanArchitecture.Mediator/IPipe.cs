72ad4416c8ab54e289133084d626099433b8b62f
James Roberts <James@Robertscom.com>
2025-07-19 13:32:26 +1000

    Add the generic IPipe interface
    #98

--- src/CleanArchitecture.Mediator/IPipe.cs
+++ src/CleanArchitecture.Mediator/IPipe.cs
@@ -38,4 +38,38 @@ namespace CleanArchitecture.Mediator
 
     }
 
+    /// <summary>
+    /// A single pipe in the pipeline.
+    /// </summary>
+    /// <typeparam name="TInputPort">The type of input port.</typeparam>
+    /// <typeparam name="TOutputPort">The type of output port.</typeparam>
+    /// <remarks>
+    /// Pipe implementations are registered as singletons, which means that only singleton and transient services should be resolved in their constructors.<br/>
+    /// Scoped services can be resolved in the <see cref="InvokeAsync(TInputPort, TOutputPort, ServiceFactory, IPipeHandle, CancellationToken)"/>
+    /// method by using the <see cref="ServiceFactory"/> parameter.
+    /// </remarks>
+    public interface IPipe<TInputPort, TOutputPort> where TInputPort : IInputPort<TOutputPort>
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        /// <summary>
+        /// Handles the relevant behaviour for this section of the pipeline.
+        /// </summary>
+        /// <param name="inputPort">The input to the pipeline.</param>
+        /// <param name="outputPort">The output mechanism for the pipeline.</param>
+        /// <param name="serviceFactory">The factory used to get the service object of the specified type.</param>
+        /// <param name="nextPipeHandle">The handle to the next pipe in the pipeline.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be cancelled.</param>
+        Task InvokeAsync(
+            TInputPort inputPort,
+            TOutputPort outputPort,
+            ServiceFactory serviceFactory,
+            IPipeHandle nextPipeHandle,
+            CancellationToken cancellationToken);
+
+        #endregion Methods
+
+    }
+
 }
