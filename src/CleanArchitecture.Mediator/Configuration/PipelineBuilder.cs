e847dd95743ed612f44f2bd6146bac1c9f0947ae
James Roberts <James@Robertscom.com>
2025-07-20 21:12:16 +1000

    Add registering open generic pipes to a pipeline
    #98

--- src/CleanArchitecture.Mediator/Configuration/PipelineBuilder.cs
+++ src/CleanArchitecture.Mediator/Configuration/PipelineBuilder.cs
@@ -75,6 +75,40 @@ namespace CleanArchitecture.Mediator.Configuration
         }
 
         /// <summary>
+        /// Adds an open generic pipe to the pipeline.
+        /// </summary>
+        /// <param name="openGenericPipeType">The <see cref="Type"/> of open generic pipe.</param>
+        /// <param name="closedGenericTypes">The types (excluding input port and output port) required to close the open generic pipe.</param>
+        /// <param name="serviceTypes">The services that are produced by the <see cref="ServiceFactory"/> within the pipe. For generic types, the generic type definition should be provided.</param>
+        /// <returns>Itself.</returns>
+        /// <exception cref="ArgumentException"><paramref name="openGenericPipeType"/> is not an open generic type.</exception>
+        /// <exception cref="ArgumentException"><paramref name="openGenericPipeType"/> does not implement <see cref="IPipe{TInputPort, TOutputPort}"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="openGenericPipeType"/> implements <see cref="IPipe{TInputPort, TOutputPort}"/> more than once.</exception>
+        /// <exception cref="ArgumentException">The TInputPort parameter of the <see cref="IPipe{TInputPort, TOutputPort}"/> implementation is not a generic parameter.</exception>
+        /// <exception cref="ArgumentException">The TOutputPort parameter of the <see cref="IPipe{TInputPort, TOutputPort}"/> implementation is not a generic parameter.</exception>
+        /// <exception cref="ArgumentException">An incorrect number of closed generic parameters are specified in the <paramref name="closedGenericTypes"/> collection.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="openGenericPipeType"/> is null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="closedGenericTypes"/> is null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="serviceTypes"/> is null.</exception>
+        public PipelineBuilder<TPipeline> AddOpenGenericPipe(Type openGenericPipeType, Type[] closedGenericTypes, params Type[] serviceTypes)
+        {
+            if (openGenericPipeType is null) throw new ArgumentNullException(nameof(openGenericPipeType));
+            if (closedGenericTypes is null) throw new ArgumentNullException(nameof(closedGenericTypes));
+            if (serviceTypes is null) throw new ArgumentNullException(nameof(serviceTypes));
+
+            // This is declared outside the PipHandleProvider function to ensure the checks are done during registration.
+            var _PipeProvider = new ClosedGenericPipeProvider(openGenericPipeType, closedGenericTypes);
+
+            this.m_PipeHandleProviders.Add((_, nextPipeHandle) => new OpenGenericPipeHandle(_PipeProvider, nextPipeHandle));
+            this.m_OnPipeAdded(openGenericPipeType);
+
+            foreach (var _ServiceType in serviceTypes)
+                this.m_OnServiceAdded(_ServiceType);
+
+            return this;
+        }
+
+        /// <summary>
         /// Adds inline behaviour to the pipeline.
         /// </summary>
         /// <param name="inlineBehaviourAsync">The behaviour of the pipe. The parameters are (inputPort, outputPort, serviceFactory, nextPipeHandleAsync, cancellationToken).</param>
