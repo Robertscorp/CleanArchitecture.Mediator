8e01e9ab3398a66d9e312c9077fc7900dac6e756
James Roberts <James@Robertscom.com>
2025-07-27 12:42:15 +1000

    Remove newline
    #100

--- sample/CleanArchitecture.Mediator.Sample/UseCases/CreateProduct/CreateProductAuthorisationEnforcer.cs
+++ sample/CleanArchitecture.Mediator.Sample/UseCases/CreateProduct/CreateProductAuthorisationEnforcer.cs
@@ -12,7 +12,6 @@ public class CreateProductAuthorisationEnforcer : IAuthorisationEnforcer<CreateP
         return Task.FromResult(!inputPort.FailAuthorisation);
     }
 
-
     #endregion Methods
 
 }
