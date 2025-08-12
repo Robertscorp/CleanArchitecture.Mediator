873e29361fd7826b76997245d37f49d9e53440e2
James Roberts <James@Robertscom.com>
2025-08-12 21:15:31 +1000

    Add ContinuationBehaviour improvements
    The improvements include:
        - Async versions of each type of ContinuationBehaviour, making the return values of async and non-async method cleaner.
        - The ability to aggregate ContinuationBehaviour based on the severity. Higher severity will be chosen over lower severity.
    
    #133

---
+++ test/CleanArchitecture.Mediator.Tests/Unit/ContinuationBehaviourTests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitecture.Mediator.Tests.Unit;
+
+public class ContinuationBehaviourTests
+{
+
+    #region - - - - - - Aggregate Tests - - - - - -
+
+    [Fact]
+    public void Aggregate_MultipleContinuationBehaviours_FirstWithHighestSeverityChosen()
+    {
+        // Arrange
+        var _Expected = ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 10);
+
+        // Act
+        var _Actual
+            = ContinuationBehaviour.Aggregate(
+                ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 1),
+                ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 5),
+                ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 9),
+                ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 6),
+                ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 3),
+                _Expected,
+                ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 10),
+                ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 10));
+
+        // Assert
+        _ = _Actual.Should().Be(_Expected);
+    }
+
+    #endregion Aggregate Tests
+
+    #region - - - - - - AggregateWith Tests - - - - - -
+
+    [Fact]
+    public void AggregateWith_ContinuationBehaviourParameterHasLowerPriority_ThisInstanceChosen()
+    {
+        // Arrange
+        var _Expected = ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 10);
+
+        // Act
+        var _Actual = _Expected.AggregateWith(ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 9));
+
+        // Assert
+        _ = _Actual.Should().Be(_Expected);
+    }
+
+    [Fact]
+    public void AggregateWith_ContinuationBehaviourParameterHasSamePriority_ThisInstanceChosen()
+    {
+        // Arrange
+        var _Expected = ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 10);
+
+        // Act
+        var _Actual = _Expected.AggregateWith(ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 10));
+
+        // Assert
+        _ = _Actual.Should().Be(_Expected);
+    }
+
+    [Fact]
+    public void AggregateWith_ContinuationBehaviourParameterHasHigherPriority_ThisInstanceChosen()
+    {
+        // Arrange
+        var _Expected = ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 11);
+
+        // Act
+        var _Actual = ContinuationBehaviour.Custom((_, _) => Task.CompletedTask, 10).AggregateWith(_Expected);
+
+        // Assert
+        _ = _Actual.Should().Be(_Expected);
+    }
+
+    #endregion AggregateWith Tests
+
+}
